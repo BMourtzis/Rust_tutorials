172f42b5133a1f0fd5360f8cf8d0c53424bc2d9f
Vasilis Mourtzis <vasilis.mourtzis@outlook.com>
2019-02-23 18:35:55 +1100

    minor changes to gitignore

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,7 +1,31 @@
 fn main() {
-    newVextor();
+    new_vector();
 }
 
-fn newVextor() {
+fn new_vector() {
+    let mut v = vec![1, 2, 3];
+
+    v.push(4);
+    v.push(5);
+
+    let third = &v[2];
+
+    v.push(4);
+
+    match v.get(2) {
+        Some(third) => print!("The thrid element is {}", third),
+        None => print!("There is no third element"),
+    }
+
+    for i in &v {
+        println!("{}", i);
+    }
+
+    for i in &mut v {
+        *i += 50;
+    }
+}
+
+fn new_string() {
     
 }
